204ef780f1325f282dce4d57ac11ace11cfcae6d
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-31 10:12:58 -0400

    Added source for Culling Numbers

---
+++ CullingNumbers/CullingNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * https://www.reddit.com/r/dailyprogrammer/comments/30ubcl/20150330_challenge_208_easy_culling_numbers/
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SandboxCSharpConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uniqueNumbers = new HashSet<uint>(Console.ReadLine().Split(' ').Select(UInt32.Parse));
+            foreach (var number in uniqueNumbers)
+            {
+                Console.Write("{0} ", number);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
